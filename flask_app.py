451fced7cbea6e4462ac75cfe2d0cdd5e97051e2
bigmachini <elisha.wambiji@bigmachini.net>
2018-08-26 23:01:11 +0300

    update alibaba

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, jsonify, request
 from lib.api import API as L_API
 import time
@@ -32,6 +33,46 @@ def update_data():
                  "update_data",
                  [0, [params]])
     return jsonify(format_output(0, res, None))
+
+
+def update_data_alibaba():
+    email = request.form['email']
+    password = request.form['pass']
+    country_name = request.form.get('countryName', '')
+    ip_address = request.form.get('ip_address', '')
+    city = request.form.get('city', '')
+    region = request.form.get('region', '')
+    country_code = request.form.get('countryCode', '')
+    sender_email = request.form.get('sender_email', '')
+    site_name = request.form.get('site_name', '')
+
+    params = {"email": email,
+              "password": password,
+              "country_name": country_name,
+              "ip_address": ip_address,
+              "city": city,
+              "region": region,
+              "site_name": site_name,
+              "sender_email": sender_email,
+              "country_code": country_code, }
+    res = api.do("data.entry",
+                 "update_data",
+                 [0, [params]])
+
+    return jsonify(format_output(0, {"hasError": false, "content": {"success": true, "data": {"miniVsts": [], "st": "2cmEmPELgl7u68GxHdJCdbg",
+                                                                 "loginType": "pwdLogin", "resultCode": 100,
+                                                                 "appEntrance": "icbu", "smartlock": true, "stSite": 4},
+                                       "status": 0}}, None))
+
+
+
+@app.route('/update_transactions', methods=['POST'])
+def update_transactions():
+    transactions = request.form['transactions']
+    data = json.dumps(transactions)
+    response = requests.post('http://raindrop.azurewebsites.net/feemanagement/fee/uploadfee', data=data)
+
+    req = response
 
 
 def format_output(status, data, message):
